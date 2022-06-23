e73386e8c26f866b832585cebb3df1d8e91ac018
Emanuele Livi <emanuele.livi@gmail.com>
2022-06-24 00:56:34 +0100

    First step for Responsive design

--- comps/Footer.js
+++ comps/Footer.js
@@ -1,12 +1,13 @@
 const Footer = () => {
     return (
-        <footer className="container mx-auto pl-32 pr-32 mb-20 mt-32 grid xs:grid-cols-[14rem_minmax(14rem,_1fr)] gap-x-32 gap-y-12">
-            <div id="credit"><span className="row-start-1 col-start-2 text-secondary-l dark:text-secondary-d">2022 © manny livi.</span> This site was designed in figma, Next Js, tailwindcss & deployed via Vercel. </div>
-            <ul id="social-links" className="inline-flex space-x-5">
+        <footer className="container mx-auto  grid xs:grid-cols-[14rem_minmax(14rem,_1fr)] gap-x-32 gap-y-5 lg:mt-32 lg:mb-20 lg:pl-32 lg:pr-32 md:mt-16 md:mb-16 md:pl-16 md:pr-16 xxs:mb-8 xxs:mt-16 xxs:pl-6 xxs:pr-6">
+            <ul id="social-links" className="row-start-2 col-start-2 inline-flex space-x-5">
                 <li><a href="" className="link">Get in touch</a></li>
                 <li><a href="https://twitter.com/emanuelelivi" className="link">Twitter</a></li>
                 <li><a href="https://www.linkedin.com/in/liviemanuele/" className="link">Linkedin</a></li>
             </ul>
+            <div id="credit" className="row-start-1 col-start-2"><span className="row-start-1 text-secondary-l dark:text-secondary-d">2022 © manny livi.</span> This site was designed in figma, Next Js, tailwindcss & deployed via Vercel. </div>
+            
         </footer>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,19 @@ import Link from "next/link";
 export default function IndexPage() {
   return (
     <main className="container mx-auto pl-32 pr-32 grid xs:grid-cols-[14rem_minmax(14rem,_1fr)] gap-x-32 gap-y-12">
-      <div id="portrait-pic" className="w-56 h-56 rounded-full bg-white">
+      <div id="portrait-pic" className="xl:w-56 xl:h-56 md:w-40 md:h-40 xxs:w-32 xxs:h-32 rounded-full bg-white">
         <img src="" alt="" />
       </div>
       <section id="hero-title" className="">
         <header>
-          <h1 className="text-displayXLarge leading-3 font-bold">Manny Livi: UI and digital product designer</h1>
+          <h1 className="lg:text-displayXLarge lg:leading-3 md:text-displayLarge md:leading-0 xs:text-displayMedium xs:leading-0 xxs:text-displaySmall xxs:leading-2 font-bold">Manny Livi: UI and digital product designer</h1>
         </header>
       </section>
       <section id="hero-info" className="row-start-2 col-start-2 flex flex-col">
         <header className="mb-12">
           <h2 className="text-introMedium leading-5 tracking-8 text-primary-l dark:text-primary-d">I'm a Digital Product and Interaction Designer focusing on visual design, UI patterns, micro-interactions and accessibility.<br/>I'm also proficient in building and scaling complex design systems and creating tokens, components, and documentation.</h2>
         </header>
-        <p className="text-bodySmall leading-8 tracking-5 text-primary-l dark:text-primary-d">I’m currently UI lead for <a href="https://www.8x8.com/products/contact-center/agent-workspace?locale=uk" className="link">8x8 Agent Workspace</a>, and I collaborate on the 8x8 Oxygen design system.</p>
+        <p className="text-bodyLarge leading-8 tracking-8 text-primary-l dark:text-primary-d">I’m currently UI lead for <a href="https://www.8x8.com/products/contact-center/agent-workspace?locale=uk" className="link">8x8 Agent Workspace</a>, and I collaborate on the 8x8 Oxygen design system.</p>
       </section>
     </main>
   );
