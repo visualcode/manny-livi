8b6189a06214497ca38e3678f1742d97b341ca9e
Emanuele Livi <emanuele.livi@gmail.com>
2022-06-23 01:14:16 +0100

    New big layout progress

--- comps/Header.js
+++ comps/Header.js
@@ -3,10 +3,10 @@ import { motion } from "framer-motion";
 
 const Header = () => {
     return (
-        <header id="main-header" className="container mx-auto">
-            <div className="">
-                <div className="text-headingMedium leading-5 font-bold text-primary-l dark:text-primary-d">Manny <span className="text-secondary-l dark:text-secondary-d">Livi</span></div>
-                <div className="text-headingXSmall leading-5 text-primary-l dark:text-primary-d">London, UK / Remote</div>
+        <header id="main-header" className="container mx-auto pl-32 pr-32 mt-20 mb-32">
+            <div id="brand-space">
+                <div className="text-headingMedium leading-5 tracking-8 font-bold text-primary-l dark:text-primary-d">Manny <span className="text-secondary-l dark:text-secondary-d">Livi</span></div>
+                <div className="text-headingXSmall leading-5 tracking-7 text-primary-l dark:text-primary-d">London, UK / Remote</div>
             </div>
         </header>
     );
--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,21 @@ import Link from "next/link";
 
 export default function IndexPage() {
   return (
-    <main className="">
-      <section id="hero">
-        <h1 className="">Manny Livi is a<br />UI and Digital Product Designer<span className="text-tertiary-l dark:text-tertiary-d">.</span></h1>
-      </section>
-      <div className="">
-        <h2 className="">I'm a Product and Interaction Designer focusing on visual design, UI patterns, micro-interactions and accessibility. I'm also an expert on building and scaling complex design systems, creating tokens, components, processes and documentation.</h2>
-        <p className="sm:mt-16 xxs:mt-8 lg:col-span-2 lg:col-start-2 xxs:col-span-3 xxs:col-start-1 text-paragraphLarge">I’m currently UI lead for the awarded <a href="https://www.8x8.com/products/contact-center/agent-workspace?locale=uk">8x8 Agent Workspace</a>, and I collaborate on the 8x8 Oxygen design system.</p>
+    <main className="container mx-auto pl-32 pr-32 grid xs:grid-cols-[14rem_minmax(14rem,_1fr)] gap-x-32 gap-y-12">
+      <div id="portrait-pic" className="w-56 h-56 bg-white">
+        <img src="" alt="" />
       </div>
+      <section id="hero-title" className="">
+        <header>
+          <h1 className="text-displayXLarge leading-3 font-bold">Manny Livi: UI and digital product designer</h1>
+        </header>
+      </section>
+      <section id="hero-info" className="row-start-2 col-start-2 flex flex-col">
+        <header className="mb-12">
+          <h2 className="text-introMedium leading-5 tracking-8 text-primary-l dark:text-primary-d">I'm a Digital Product and Interaction Designer focusing on visual design, UI patterns, micro-interactions and accessibility.<br/>I'm also proficient in building and scaling complex design systems and creating tokens, components, and documentation.</h2>
+        </header>
+        <p className="text-bodySmall leading-8 tracking-5 text-primary-l dark:text-primary-d">I’m currently UI lead for the awarded <a href="https://www.8x8.com/products/contact-center/agent-workspace?locale=uk">8x8 Agent Workspace</a>, and I collaborate on the 8x8 Oxygen design system.</p>
+      </section>
     </main>
   );
 }
