fe001d18f8d71b54c937781e3e2dc21c04ecf33d
Emanuele Livi <emanuele.livi@8x8.com>
2022-06-23 16:02:25 +0100

    New update includes Hero and footer progress

--- comps/Footer.js
+++ comps/Footer.js
@@ -1,10 +1,11 @@
 const Footer = () => {
     return (
-        <footer className="">
-            <div className=""></div>
-            <ul className="">
-                <li><a href="https://twitter.com/emanuelelivi" className="link text-paragraphLarge leading-none">Twitter</a></li>
-                <li><a href="https://www.linkedin.com/in/liviemanuele/" className="link text-paragraphLarge leading-none">Linkedin</a></li>
+        <footer className="container mx-auto pl-32 pr-32 mb-20 mt-32 grid xs:grid-cols-[14rem_minmax(14rem,_1fr)] gap-x-32 gap-y-12">
+            <div id="credit"><span className="row-start-1 col-start-2 text-secondary-l dark:text-secondary-d">2022 © manny livi.</span> This site was designed in figma, Next Js, tailwindcss & deployed via Vercel. </div>
+            <ul id="social-links" className="inline-flex space-x-5">
+                <li><a href="" className="link">Get in touch</a></li>
+                <li><a href="https://twitter.com/emanuelelivi" className="link">Twitter</a></li>
+                <li><a href="https://www.linkedin.com/in/liviemanuele/" className="link">Linkedin</a></li>
             </ul>
         </footer>
     );
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function IndexPage() {
   return (
     <main className="container mx-auto pl-32 pr-32 grid xs:grid-cols-[14rem_minmax(14rem,_1fr)] gap-x-32 gap-y-12">
-      <div id="portrait-pic" className="w-56 h-56 bg-white">
+      <div id="portrait-pic" className="w-56 h-56 rounded-full bg-white">
         <img src="" alt="" />
       </div>
       <section id="hero-title" className="">
@@ -16,7 +16,7 @@ export default function IndexPage() {
         <header className="mb-12">
           <h2 className="text-introMedium leading-5 tracking-8 text-primary-l dark:text-primary-d">I'm a Digital Product and Interaction Designer focusing on visual design, UI patterns, micro-interactions and accessibility.<br/>I'm also proficient in building and scaling complex design systems and creating tokens, components, and documentation.</h2>
         </header>
-        <p className="text-bodySmall leading-8 tracking-5 text-primary-l dark:text-primary-d">I’m currently UI lead for the awarded <a href="https://www.8x8.com/products/contact-center/agent-workspace?locale=uk">8x8 Agent Workspace</a>, and I collaborate on the 8x8 Oxygen design system.</p>
+        <p className="text-bodySmall leading-8 tracking-5 text-primary-l dark:text-primary-d">I’m currently UI lead for <a href="https://www.8x8.com/products/contact-center/agent-workspace?locale=uk" className="link">8x8 Agent Workspace</a>, and I collaborate on the 8x8 Oxygen design system.</p>
       </section>
     </main>
   );
