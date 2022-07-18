ff3709dbd66c13fa5be3f2c2e2031e78bb800e06
Emanuele Livi <emanuele.livi@gmail.com>
2022-07-18 23:21:41 +0000

    Update Footer.js and tailwind.config.js

--- comps/Footer.js
+++ comps/Footer.js
@@ -1,38 +1,60 @@
 import { motion } from "framer-motion";
 
 const elementAppear = {
-    hidden: {
-        opacity: 0,
-        y: 16
-    },
-    visible: {
-        opacity: 1,
-        y: 0,
-        transition: {
-            type: 'spring',
-            delay: 0.8,
-            stifness: 238,
-            Damping: 30,
-            Mass: 1
-        }
+  hidden: {
+    opacity: 0,
+    y: 16
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      delay: 0.8,
+      stifness: 238,
+      Damping: 30,
+      Mass: 1
     }
-}
+  }
+};
 
 const Footer = () => {
-    return (
-        <motion.footer 
-            variants={ elementAppear }
-            initial='hidden'
-            animate='visible'
-            className="mx-auto grid xxs:grid-flow-row xxs:auto-cols-fr xxs:grid-cols-1 xxs:grid-rows-1 xxs:gap-y-8 xxs:pl-6 xxs:pr-6 xxs:mt-16 xxs:mb-8 xs:mt-16 xs:mb-16 xs:pl-16 xs:pr-16 lg:mt-32 lg:mb-20 lg:pl-32 lg:pr-32 xxl:container">
-            <ul id="social-links" className="inline-flex xxs:gap-4">
-                <li><a href="mailto:emanuele.livi@gmail.com?subject=Request from mannylivi.com&body=" className="link xxs:text-bodySmall">Get in touch</a></li>
-                <li><a href="https://twitter.com/emanuelelivi" className="link">Twitter</a></li>
-                <li><a href="https://www.linkedin.com/in/liviemanuele/" className="link">Linkedin</a></li>
-            </ul>
-            <div id="credit" className="xxs:leading-9 xxs:tracking-7"><span className="row-start-1 text-secondary-l dark:text-secondary-d">2022 © manny livi.</span> This site was designed in figma, Next Js, tailwindcss & deployed via Vercel. </div>
-        </motion.footer>
-    );
-}
+  return (
+    <motion.footer
+      variants={elementAppear}
+      initial="hidden"
+      animate="visible"
+      className="mx-auto grid xxs:grid-flow-row xxs:auto-cols-fr xxs:grid-cols-1 xxs:grid-rows-1 xxs:gap-y-8 xxs:pl-6 xxs:pr-6 xxs:mt-16 xxs:mb-8 xs:mt-16 xs:mb-16 xs:pl-16 xs:pr-16 lg:mt-32 lg:mb-20 lg:pl-32 lg:pr-32 xxl:container"
+    >
+      <ul id="social-links" className="inline-flex xxs:gap-4">
+        <li>
+          <a
+            href="mailto:emanuele.livi@gmail.com?subject=Request from mannylivi.com&body="
+            className="link xxs:text-bodySmall"
+          >
+            Get in touch
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/emanuelelivi" className="link">
+            Twitter
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/liviemanuele/" className="link">
+            Linkedin
+          </a>
+        </li>
+      </ul>
+      <div id="credit" className="xxs:leading-9 xxs:tracking-7">
+        <span className="row-start-1 text-secondary-l dark:text-secondary-d">
+          2022 © manny livi.
+        </span>{" "}
+        This site was designed in Figma, Next Js, Tailwindcss & deployed via
+        Vercel.{" "}
+      </div>
+    </motion.footer>
+  );
+};
 
-    export default Footer;
+export default Footer;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,119 +1,116 @@
 module.exports = {
   content: {
     enabled: true,
-    content: [
-      "./pages/**/*.js",
-      "./comps/**/*.js"
-    ]
+    content: ["./pages/**/*.js", "./comps/**/*.js"]
   },
 
   theme: {
     screens: {
-      xxs: '320px',
-      xs: '480px',
-      sm: '568px',
-      md: '768px',
-      lg: '992px',
-      xl: '1312px',
-      xxl: '1440px'
+      xxs: "320px",
+      xs: "480px",
+      sm: "568px",
+      md: "768px",
+      lg: "992px",
+      xl: "1312px",
+      xxl: "1440px"
     },
     extend: {
       fontFamily: {
-        sans: "-apple-system, BlinkMacSystemFont, avenir next, avenir, segoe ui, helvetica neue, helvetica, Ubuntu, roboto, noto, arial, sans-serif",
+        sans: "-apple-system, BlinkMacSystemFont, avenir next, avenir, segoe ui, helvetica neue, helvetica, Ubuntu, roboto, noto, arial, sans-serif"
         // serif: "-apple-system-ui-serif, ui-serif, 'Georgia', serif",
       },
       fontSize: {
-        'displayXLarge': '5.16rem',
-        'displayLarge': '4.3rem',
-        'displayMedium': '3.583rem',
-        'displaySmall': '2.986rem',
-        'displayXSmall': '2.488rem',
-        'headingMedium': '1.728rem',
-        'headingSmall': '1.44rem',
-        'headingXSmall': '0.833rem',
-        'bodyLarge': '1.2rem',
-        'bodySmall': '1rem',
-        'bodyXSmall': '0.833rem',
-        'linkLarge': '1.2rem',
-        'linkSmall': '1rem',
-        'introMedium': '1.728rem',
-        'introSmall': '1.44rem'
+        displayXLarge: "5.16rem",
+        displayLarge: "4.3rem",
+        displayMedium: "3.583rem",
+        displaySmall: "2.986rem",
+        displayXSmall: "2.488rem",
+        headingMedium: "1.728rem",
+        headingSmall: "1.44rem",
+        headingXSmall: "0.833rem",
+        bodyLarge: "1.2rem",
+        bodySmall: "1rem",
+        bodyXSmall: "0.833rem",
+        linkLarge: "1.2rem",
+        linkSmall: "1rem",
+        introMedium: "1.728rem",
+        introSmall: "1.44rem"
       },
       lineHeight: {
-        '0': '1em',
-        '1': '1.2em',
-        '2': '1.125em',
-        '3': '1.11em',
-        '4': '1.115em',
-        '5': '1.388em',
-        '6': '1.333em',
-        '7': '1.71em',
-        '8': '1.6em',
-        '9': '1.43em',
+        0: "1em",
+        1: "1.2em",
+        2: "1.125em",
+        3: "1.11em",
+        4: "1.115em",
+        5: "1.388em",
+        6: "1.333em",
+        7: "1.71em",
+        8: "1.6em",
+        9: "1.43em"
       },
       letterSpacing: {
-        '0': '-1.33px',
-        '1': '-1.06px',
-        '2': '-0.85px',
-        '3': '-0.71px',
-        '4': '-0.58px',
-        '5': '-0.45px',
-        '6': '-0.31px',
-        '7': '-0.15',
-        '8': '0.38px',
-        '9': '0'
+        0: "-1.33px",
+        1: "-1.06px",
+        2: "-0.85px",
+        3: "-0.71px",
+        4: "-0.58px",
+        5: "-0.45px",
+        6: "-0.31px",
+        7: "-0.15",
+        8: "0.38px",
+        9: "0"
       },
       textColor: {
         /* Light Mode colours */
-        'text-primary-l': '#2F3440',
-        'text-onPrimary-l': '#F2EDE4',
-        'text-secondary-l': '666A73',
-        'text-onSecondary-l': '#F2EDE4',
-        'text-tertiary-l': '#D04506',
+        "text-primary-l": "#2F3440",
+        "text-onPrimary-l": "#F2EDE4",
+        "text-secondary-l": "666A73",
+        "text-onSecondary-l": "#F2EDE4",
+        "text-tertiary-l": "#D04506",
         /* Dark Mode colours */
-        'text-primary-d': '#F2EDE4',
-        'text-onPrimary-d': '#F2EDE4',
-        'text-secondary-d': '666A73',
-        'text-onSecondary-d': '#F2EDE4',
-        'text-tertiary-d': '#D04506',
+        "text-primary-d": "#F2EDE4",
+        "text-onPrimary-d": "#F2EDE4",
+        "text-secondary-d": "666A73",
+        "text-onSecondary-d": "#F2EDE4",
+        "text-tertiary-d": "#D04506"
       },
       colors: {
         /* Light Mode colours */
-        'primary-l': '#2F3440',
-        'onPrimary-l': '#F2EDE4',
-        'secondary-l': '#666A73',
-        'onSecondary-l': 'F2EDE4',
-        'tertiary-l': '#D04506',
-        'onTertiary-l': '#FDFDFC',
-        'surface-l': '#F2EDE4',
-        'onSurface-l': '#2F3440',
-        'onSurfaceInverse-l': '#D9D1C7',
-        'outline-l': '#D9D1C7',
+        "primary-l": "#2F3440",
+        "onPrimary-l": "#F2EDE4",
+        "secondary-l": "#666A73",
+        "onSecondary-l": "F2EDE4",
+        "tertiary-l": "#D04506",
+        "onTertiary-l": "#FDFDFC",
+        "surface-l": "#F2EDE4",
+        "onSurface-l": "#2F3440",
+        "onSurfaceInverse-l": "#D9D1C7",
+        "outline-l": "#D9D1C7",
         /* Dark Mode colours */
-        'primary-d': '#F2EDE4',
-        'onPrimary-d': '#2F3440',
-        'secondary-d': '#A6B1B9',
-        'onSecondary-d': '#020203',
-        'tertiary-d': '#FF763A',
-        'onTertiary-d': '#020203',
-        'surface-d': '#2F3440',
-        'onSurface-d': '#F2EDE4',
-        'onSurfaceInverse-d': '#585D6B',
-        'outline-d': '#585D6B',
+        "primary-d": "#F2EDE4",
+        "onPrimary-d": "#2F3440",
+        "secondary-d": "#A6B1B9",
+        "onSecondary-d": "#020203",
+        "tertiary-d": "#FF763A",
+        "onTertiary-d": "#020203",
+        "surface-d": "#2F3440",
+        "onSurface-d": "#F2EDE4",
+        "onSurfaceInverse-d": "#585D6B",
+        "outline-d": "#585D6B"
       },
       dropShadow: {
-        'portrait': [
-          '0px 29px 12px rgba(0, 0, 0, 0.02)', 
-          '0px 16px 10px rgba(0, 0, 0, 0.06)',
-          '0px 7px 7px rgba(0, 0, 0, 0.1)', 
-          '0px 2px 4px rgba(0, 0, 0, 0.12)', 
-          '0px 0px 0px rgba(0, 0, 0, 0.12)'
+        portrait: [
+          "0px 29px 12px rgba(0, 0, 0, 0.02)",
+          "0px 16px 10px rgba(0, 0, 0, 0.06)",
+          "0px 7px 7px rgba(0, 0, 0, 0.1)",
+          "0px 2px 4px rgba(0, 0, 0, 0.12)",
+          "0px 0px 0px rgba(0, 0, 0, 0.12)"
         ]
       }
-    },
+    }
   },
   plugins: [
-    require('tailwindcss')('./tailwind.config.js'),
-    require('autoprefixer')
+    require("tailwindcss")("./tailwind.config.js"),
+    require("autoprefixer")
   ]
 };
